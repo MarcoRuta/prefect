83cd5e79717ad562d845ff7260d3add1378adef7
Ubuntu <e.seder@nextworks.it>
2023-11-09 09:18:43 +0000

    s3

--- flows/test_boto.py
+++ flows/test_boto.py
@@ -1,10 +1,8 @@
-from prefect import flow, get_run_logger, tags
-from prefect import task
 from prefect_aws import MinIOCredentials
 from prefect_aws.s3 import S3Bucket
 import pandas as pd
 import numpy as np
-@task
+
 def fetch_data():
 
     minio_credentials = MinIOCredentials(
@@ -13,17 +11,17 @@ def fetch_data():
     )
     s3_client = minio_credentials.get_boto3_session().client(
        service_name="s3",
-       endpoint_url="http://10.30.8.228:9000"
+       endpoint_url="http://10.30.8.228:9001"
     )
     s3_bucket = S3Bucket(
         bucket_name="test1",  # must exist
         minio_credentials=MinIOCredentials(minio_root_user = "minio", minio_root_password = "minio123"),
-        endpoint_url="http://10.30.8.228:9000"
+        endpoint_url="http://10.30.8.228:9001"
     )
 
     s3_resource = minio_credentials.get_boto3_session().resource(
        service_name="s3",
-       endpoint_url="http://10.30.8.228:9000"
+       endpoint_url="http://10.30.8.228:9001"
     )
     s3_object = s3_resource.Object(
        bucket_name='test1',
@@ -32,18 +30,9 @@ def fetch_data():
 
 #    s3_client.download_file('test1', 'data.csv', 'data.csv')
 #    s3_client.download_file(Bucket="test1", Key="data.csv", Filename="data.csv")
-#    s3_object.download_file(Filename='./data.csv')
-    s3_bucket.download_object_to_path("data.csv", "data.csv")
+    s3_object.download_file(Filename='./data.csv')
+    #s3_bucket.download_object_to_path("data.csv", "data.csv")
     data = pd.read_csv("data.csv")
-    return data
+    print(data.head())
 
-@flow
-def hello(name: str = "Marvin"):
-    data = fetch_data()
-    logger = get_run_logger()
-    logger.info(f"Hello, {name}!")
-    logger.info(f"minIO data: {data.head(10)}!")
-
-if __name__ == "__main__":
-    with tags("local"):
-        hello()
+fetch_data()
