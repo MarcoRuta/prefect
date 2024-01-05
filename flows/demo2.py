fa90070f43df66038b02ef5833a8fda3d4191cfc
ErinElizabethSeder <39697981+ErinElizabethSeder@users.noreply.github.com>
2024-01-05 13:07:26 +0100

    Update demo2.py

--- flows/demo2.py
+++ flows/demo2.py
@@ -82,17 +82,17 @@ def train_model(logger, data, mlflow_experiment_id, lr):
         mlflow.log_metric("r2", r2)
         mlflow.log_metric("mae", mae)
 
-        mlflow.sklearn.log_model(lr, "model")
+        #mlflow.sklearn.log_model(lr, "model")
     # Infer the model signature
-        #y_pred = lr.predict(test_x)
-        #signature = infer_signature(test_x, y_pred)
+        y_pred = lr.predict(test_x)
+        signature = infer_signature(test_x, y_pred)
     # Log the sklearn model and register as version 1
-        #mlflow.sklearn.log_model(
-            #sk_model=lr,
-            #artifact_path="sklearn-model",
-            #signature=signature,
-            #registered_model_name="demo-linear-regression-model",
-        #)
+        mlflow.sklearn.log_model(
+            sk_model=lr,
+            artifact_path="sklearn-model",
+            signature=signature,
+            registered_model_name="demo-linear-regression-model",
+        )
     
 
 @flow
